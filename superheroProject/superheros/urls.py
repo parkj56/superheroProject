e355cd87b7ffdd9bd35de1d10da15b4324763af4
DESKTOP-EJDVHOT\parkj <park.james1030@gmail.com>
2021-09-26 23:50:01 -0500

    created a readable list of superheros

--- superheroProject/superheros/urls.py
+++ superheroProject/superheros/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
 from . import views
 app_name='superheros'
 urlpatterns=[
-    path('index',views.index, name='index')
+    path('',views.index, name='index'),
+    path('<int:hero_id>/', views.detail, name='detail'),
+    path('new/', views.create, name='create')
 ]
--- superheroProject/superheros/views.py
+++ superheroProject/superheros/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Superhero
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello world")
+    all_heros = Superhero.objects.all()
+    context = {
+        'all_heros': all_heros
+    }
+    return render(request,'superheros/index.html', context)
+
+def detail(request, hero_id):
+    single_hero= Superhero.objects.get(pk='hero_id')
+    context={
+        'single_hero': single_hero
+    }
+    return render (request,'superheros/detail.html',context)
